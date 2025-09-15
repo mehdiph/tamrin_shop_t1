ddc151f042addae8c2258b3b7df9f9f45c8db01c
mehdiph <mehdi.teaching1996@gmail.com>
2025-09-15 19:09:57 +0330

    Update models.py
    edit class name

--- product/models.py
+++ product/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class coment (models.Model):
-    name =models.CharField(max_length=100)
+class Comment (models.Model):
+    name = models.CharField(max_length=100)
     email = models.EmailField()
     text = models.TextField()
-    created= models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
     
