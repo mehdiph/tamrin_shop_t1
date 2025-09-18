6727df3ec1ca7e0cb6ac6d4e19354eda4c0c955b
mehdi <mehdi@gmail.com>
2025-09-18 11:33:37 +0330

    create views for from logic

--- product/forms.py
+++ product/forms.py
@@ -1,9 +1,11 @@
 from django import forms 
 
-class CommentForm(forms.ModelForm):
-    name = forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'نام شما'}),
-    email = forms.EmailInput(attrs={'class': 'form-input', 'placeholder': 'ایمیل شما'}),
-    text = forms.Textarea(attrs={'class': 'form-textarea', 'placeholder': 'نظر شما درباره محصول', 'rows': 4}),
-
-
-
+class CommentForm(forms.Form):
+    name = forms.CharField(
+        label='Name',
+        widget=forms.TextInput(attrs={
+            'class': 'form-input',
+            'placeholder': 'نام شما'
+            })),
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-input', 'placeholder': 'ایمیل شما'})),
+    text = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-textarea', 'placeholder': 'نظر شما درباره محصول', 'rows': 4})),
--- product/views.py
+++ product/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .forms import CommentForm
 
 # Create your views here.
+def comment_form(request):
+    if request.method == 'POST':
+        pass
+    else:
+        print(request.method)
+        form = CommentForm()
+        print(form.errors)
+    return render(request, 'product/product_commant.html', {'form': form})
